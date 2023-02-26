6ecf48c8f4425ce8c26f5f6cfc6b13bc9e5f0a3f
bueti <me@bueti-online.ch>
2023-02-26 14:01:44 +0100

    show backups in tableview

--- pkg/backup/list.go
+++ pkg/backup/list.go
@@ -5,9 +5,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"strconv"
 
 	"bocker.software-services.dev/pkg/config"
 	"bocker.software-services.dev/pkg/docker"
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type Layer struct {
@@ -54,6 +59,14 @@ type ListTagsResponse struct {
 	Results  []Response `json:"results"`
 }
 
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+type model struct {
+	table table.Model
+}
+
 func List(app config.Application) error {
 	c, err := docker.NewHTTPClient(app)
 	if err != nil {
@@ -76,12 +89,75 @@ func List(app config.Application) error {
 			return err
 		}
 
-		for _, v := range tags.Results {
-			fmt.Println(v.Name, v.FullSize, v.LastUpdaterUsername)
+		columns := []table.Column{
+			{Title: "ID", Width: 10},
+			{Title: "Name", Width: 20},
+			{Title: "Last Updated", Width: 30},
+			{Title: "Size", Width: 10},
 		}
+
+		var rows []table.Row
+
+		for _, v := range tags.Results {
+			size := float64(v.FullSize) / (1 << 20)
+			sizeStr := fmt.Sprintf("%.2f MiB", size)
+			rows = append(rows, []string{strconv.Itoa(v.ID), v.Name, v.LastUpdated, sizeStr})
+		}
+
+		t := table.New(
+			table.WithColumns(columns),
+			table.WithRows(rows),
+			table.WithFocused(true),
+			table.WithHeight(7),
+		)
+
+		s := table.DefaultStyles()
+		s.Header = s.Header.
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderForeground(lipgloss.Color("240")).
+			BorderBottom(true).
+			Bold(false)
+		s.Selected = s.Selected.
+			Foreground(lipgloss.Color("229")).
+			Background(lipgloss.Color("57")).
+			Bold(false)
+		t.SetStyles(s)
+		m := model{t}
+		if _, err := tea.NewProgram(m).Run(); err != nil {
+			fmt.Println("Error running program:", err)
+			os.Exit(1)
+		}
+
 	} else {
 		app.ErrorLog.Println(resp.StatusCode)
 	}
 
 	return nil
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			if m.table.Focused() {
+				m.table.Blur()
+			} else {
+				m.table.Focus()
+			}
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			return m, tea.Quit
+		}
+	}
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return baseStyle.Render(m.table.View()) + "\n"
 }
